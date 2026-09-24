07dcc1c4b6678169e4166bd759b2a47feeed186e
laurentidevweb <gabylaurenti01@gmail.com>
2026-09-23 22:57:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ const introMusic = document.createElement("audio");
 introMusic.src = CONFIG.music;
 introMusic.preload = "auto";
 introMusic.loop = true;
-introMusic.volume = 0.14;
+introMusic.volume = window.innerWidth <= 600 ? 0.14 : 0.14;
 introMusic.setAttribute("aria-hidden", "true");
 
 document.body.appendChild(introMusic);
